e7b4fefadb9b9b1b0bc635205358e1b980025807
Brian OMeara <omeara.brian@gmail.com>
2018-03-26 17:17:22 -0400

    vcv tweak

--- R/bmhyb.r
+++ R/bmhyb.r
@@ -680,6 +680,67 @@ BrissetteEtAlCorrection <- function(V.modified, min.eigenvalue=1e-6, max.attempt
   return(V.corrected)
 }
 
+GetVModifiedOriginalIsh <- function(x, phy, flow, actual.params, measurement.error=NULL) {
+	bt <- 1
+	vh <- 0
+	sigma.sq <- x[1]
+	mu <- x[2]
+  SE <- 0
+  if(is.null(measurement.error)) {
+	   SE <- x[length(x)]
+}
+	bt.location <- which(names(actual.params)=="bt")
+	if(length(bt.location)==1) {
+		bt<-x[bt.location]
+	}
+	vh.location <- which(names(actual.params)=="vh")
+	if(length(vh.location)==1) {
+		vh<-x[vh.location]
+	}
+	times.original <-vcv.phylo(phy, model="Brownian") #is the initial one based on the tree alone, so just time
+	V.original <- sigma.sq * times.original
+	V.modified <- V.original
+	for (flow.index in sequence(dim(flow)[1])) {
+		recipient.index <- which(rownames(V.modified)==flow$recipient[flow.index])
+    gamma <- flow$gamma[flow.index]
+		if(length(recipient.index)!=1) {
+			stop(paste("Tried to find ", flow$recipient[flow.index], " but instead found ", paste(rownames(V.modified)[recipient.index], sep=" ", collapse= " "), "; make sure the taxon names in the flow dataframe recipient match that of your tree", sep=""))
+		}
+		donor.index <- which(rownames(V.modified)==flow$donor[flow.index])
+		if(length(donor.index)!=1) {
+			stop(paste("Tried to find ", flow$donor[flow.index], " but instead found ", paste(rownames(V.modified)[donor.index], sep=" ", collapse= " "), "; make sure the taxon names in the flow dataframe donor match that of your tree", sep=""))
+		}
+		V.modified[recipient.index, donor.index] <- (1-gamma) * V.original[recipient.index, donor.index] + (gamma) * (flow$time.from.root.recipient[flow.index]) * sigma.sq #covariance is the weighted sum of the covariance from evolution along the tree plus evolution along the migration path
+		V.modified[donor.index, recipient.index] <- V.modified[recipient.index, donor.index]
+		#covariance managed, now to manage the variance
+		V.modified[recipient.index, recipient.index] <- (V.original[recipient.index, recipient.index] -  sigma.sq*flow$time.from.root.recipient[flow.index])+ (gamma^2 + (1- gamma)^2) * (flow$time.from.root.recipient[flow.index])*sigma.sq +2*gamma*(1-gamma)*V.original[recipient.index, donor.index]  + vh
+        #this is variance for the hybrid. See math derivation at https://github.com/bomeara/bmhyb/issues/1
+    if(dim(flow)[1]>1){
+    flow.1.index<-flow.index
+    recipient.1.index <- which(rownames(V.modified) == flow$recipient[flow.1.index])
+    donor.1.index<-donor.index
+    for (flow.2.index in sequence(dim(flow)[1])) {
+      recipient.2.index<-which(rownames(V.modified)==flow$recipient[flow.2.index])
+      donor.2.index<-which(rownames(V.modified)==flow$donor[flow.2.index])
+      # case 1: (same donor) flow came in "before" speciation
+      if(flow.1.index!=flow.2.index && flow$donor[flow.1.index]==flow$donor[flow.2.index] && V.original[recipient.1.index,recipient.2.index]> max(flow$time.from.root.recipient[flow.1.index],flow$time.from.root.recipient[flow.2.index] ) ){
+         V.modified[recipient.1.index, recipient.2.index]<-(V.original[recipient.1.index,recipient.2.index] - sigma.sq*flow$time.from.root.recipient[flow.1.index]) + (1-gamma)^2*sigma.sq*flow$time.from.root.recipient[flow.1.index] + gamma^2*sigma.sq*flow$time.from.root.recipient[flow.2.index] + gamma*(1-gamma)*sigma.sq*(V.original[recipient.1.index,donor.index]+V.original[recipient.2.index,donor.index])
+         }#end case 1
+      # case 2: (same donor) flow came in "after" speciation
+      if(flow.1.index!=flow.2.index && flow$donor[flow.1.index]==flow$donor[flow.2.index] && V.original[recipient.1.index,recipient.2.index] < min(flow$time.from.root.recipient[flow.1.index], flow$time.from.root.recipient[flow.2.index]) ){
+        V.modified[recipient.1.index, recipient.2.index]<- (1-gamma)^2*sigma.sq*flow$time.from.root.recipient[flow.1.index] + gamma^2*sigma.sq*flow$time.from.root.recipient[flow.2.index]+gamma*(1-gamma)*sigma.sq*(V.original[recipient.1.index,donor.index]+V.original[recipient.2.index,donor.index])
+        }#end case 2
+      }#end for flow.index.2
+    }#end if(dim(flow)[1]>1)
+	}
+  if(is.null(measurement.error)) {
+	   diag(V.modified) <- diag(V.modified)+SE^2
+  } else {
+    diag(V.modified) <- diag(V.modified)+measurement.error^2
+  }
+	return(V.modified)
+}
+
 GetVModified <- function(x, phy, flow, actual.params, measurement.error=NULL) {
 	bt <- 1
 	vh <- 0
