89b4049487a91a1cf193bdcba86b8e95d5777048
djhwueng <djhwueng@umail.iu.edu>
2019-09-15 08:34:04 +0800

    add existing file

--- tests/testthat/test_condition.R
+++ tests/testthat/test_condition.R
@@ -37,6 +37,7 @@ test_that("Simulation works", {
 
 test_that("VCV well formed", {
   utils::data("cichlid")
+<<<<<<< Updated upstream
   VCV <- ComputeVCV(cichlid$phy.graph)
   expect_gte(kappa(VCV), 1)
 })
@@ -117,6 +118,28 @@ test_that("Issue 15 resolved", {
 #   expect_warning(calculate.likelihood.result <- CalculateLikelihood(x=parameters.to.try , data=cichlid$data, phy=cichlid$phy, flow=cichlid$flow, allow.extrapolation=FALSE, do.kappa.check=TRUE, measurement.error=0))
 #   #V.modified <- GetVModified(x=x, phy=cichlid$phy, flow=cichlid$flow, actual.params=free.parameters[which(free.parameters)], measurement.error=NULL)
 # #  matrix.condition <- kappa(V.modified, exact=TRUE) high
+=======
+  result <- BMhyb(cichlid$data, cichlid$phy, cichlid$flow, n.points=100)
+  expect_equal(class(result), "data.frame")
+  expect_equal(nrow(result), 4)
+  expect_equal(ncol(result), 22)
+
+})
+
+#
+# test_that("MergingTrees", {
+#   # idea is a network, decomposed into trees -- does it come back
+#   phy1 <- ape::read.tree(text="(((A:2,B:2):1,C:3):5,D:8);")
+#   phy2 <- ape::read.tree(text="(((B:2,C:2):1,A:3):5,D:8);")
+#   phy.graph <- MergeTreesIntoPhyGraph(c(phy1, phy2))
+#   free.parameters<-rep(TRUE, 5)
+#   names(free.parameters) <- c("sigma.sq", "mu", "bt", "vh", "SE")
+#   free.parameters[which(names(free.parameters)=="bt")]<-FALSE
+#   free.parameters[which(names(free.parameters)=="vh")]<-FALSE
+#   x <- c(1, 0, 1)
+#   results <- GetVandMFromIgraph(x, phy.graph, free.parameters)
+#   expect_equal(nrow(results$V.modified), 4)
+>>>>>>> Stashed changes
 # })
 #
 # test_that("ConditionBadNicotiana", {
